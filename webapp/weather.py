90bbdc2b7aea7febb180728f081f2f14d7d75766
Yuliua Pak <3511521@gmail.com>
2019-03-31 19:30:29 +0300

    Рефакторинг

--- webapp/weather.py
+++ webapp/weather.py
@@ -1,10 +1,11 @@
+from flask import current_app
 import requests
 
 
 def weather_by_city(city_name):
-    weather_url = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
+    weather_url = current_app.config['WEATHER_URL']
     params = {
-        'key': '8fcb8315b81043fdb4694112192403',
+        'key': current_app.config['WEATHER_API_KEY'],
         'q': city_name,
         'format': 'json',
         'num_of_days': 1,
